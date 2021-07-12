3d7d560d8a2d9aaaf588bef6ca166ad4530cbbfa
John Mettraux <jmettraux@gmail.com>
2021-07-13 06:19:26 +0900

    Abbreviate Mark to m

--- bak/monetary.rb
+++ bak/monetary.rb
@@ -4,10 +4,11 @@ require 'yaml'
 h = {}
 h[:b] = [ 'bezant', 'au', 'bizantius aureus', 7 ]
 h[:l] = [ 'pound', 'ag', 'libra', 20 ]
-h[:mk] = [ 'mark', 'ag', 'marca', 10 ]
+h[:m] = [ 'mark', 'ag', 'marca', 10 ]
 h[:s] = [ 'shilling', 'bz', 'solidus', 1 ]
 h[:d] = [ 'pence', 'bz', 'denarius', '12d' ]
-#h[:ml] = [ 'pence', 'bz', 'denarius', '12d' ]
+#h[:ml] = [ 'maille', 'bz', 'maille', '24ml' ] # half a penny
+#h[:f] = [ 'ferlin', 'bz', 'ferlin', '48ml' ] # quarter of a penny
 
 puts YAML.dump(h)
 
