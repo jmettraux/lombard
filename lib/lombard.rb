4142543713480c5e0c1e6be6787f32ea49c4dcf4
John Mettraux <jmettraux@gmail.com>
2021-07-15 13:54:54 +0900

    Merge rota cloth files

--- lib/lombard.rb
+++ lib/lombard.rb
@@ -98,7 +98,7 @@ class Lombard
 
         ta << [
           e[:kat], e[:en],
-          e[:extra].to_s.match?(/\d+(kg|l|m\d+)?/) ? e[:extra] : '',
+          ar(e[:extra].to_s.match?(/\d+(kg|l|m\d+)?/) ? e[:extra] : ''),
           ar(e[:value]), ar(e[:v]),
           *rs.collect { |ee| (e[:r] && ee == '1.0') ? 'R<<<' : ar(ee) } ]
       end
